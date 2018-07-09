f494a10693d77a8f2c5494f3cdbdcb2f028fc043
Cao <antonio.cao@yale.edu>
2018-07-09 18:27:42 +0000

    new

--- machine_learning/fully_connected/multi_linear.py
+++ machine_learning/fully_connected/multi_linear.py
@@ -27,6 +27,26 @@ def load_input(train_path):
     
     return train_x, train_y
 
+def expand_2(input_param_1, batch_size, num_param):
+    for row in range(batch_size):
+        param_tmp = input_param_1[row,:]
+        param_tmp = tf.reshape(param_tmp, [1, -1])
+        param_mat = tf.matmul(tf.transpose(param_tmp),param_tmp)
+
+        for i in range(num_param):
+            param_tmp2 = tf.reshape(param_mat[i,i:], [1,-1])
+            param_tmp = tf.concat([param_tmp,param_tmp2], 1)
+        param_tmp = tf.reshape(param_tmp, [-1])
+
+        try:
+            input_parameter = tf.concat([input_parameter,param_tmp[None, :]],0)
+            input_parameter = tf.reshape(input_parameter, [row+1, -1])
+        except:
+            input_parameter = param_tmp
+            input_parameter = tf.reshape(input_parameter, [row+1, -1])
+    
+    return input_parameter
+
 def fl_linear(param, batch_size, num_param):
     with tf.variable_scope("fl_linear", reuse=True):
         input_param = tf.placeholder(name="input_param", dtype=tf.float32, shape=(batch_size, num_param))
@@ -40,25 +60,15 @@ def mat_mul(param, batch_size, num_param):
     with tf.variable_scope("mat_mul", reuse=True):
         input_param_1 = tf.placeholder(name="input_param_1", dtype=tf.float32, shape=(batch_size, num_param))
         input_volume = tf.placeholder(name="input_volume", dtype=tf.float32, shape=(batch_size))
-        #input_param_2 = tf.square(input_param_1, name="input_param_2")
-        for row in range(batch_size):
-            param_tmp = input_param_1[row,:]
-            param_tmp = tf.reshape(param_tmp, [1, -1])
-            param_mat = tf.matmul(tf.transpose(param_tmp), param_tmp)
-            
-            for i in range(num_param):
-                param_tmp2 = tf.reshape(param_mat[i,i:], [1,-1])
-                param_tmp = tf.concat([param_tmp,param_tmp_2], 1)
-            try:
-                input_parameter = tf.stack([input_paramter, param_tmp])
-            except:
-                input_parameter = param_tmp
+        
+        input_parameter = expand_2(input_param_1, batch_size, num_param)
+        num_param = int(num_param + num_param*(num_param+1)/2)
 
         # Layer 1:
         # Input: (batch_size, num_param*2)
         # W1: (num_param*2, 1), b: (batch_size, 1)
         # Output: (batch_size, 1)
-        W1 = tf.Variable(tf.random_normal([num_param*2, 1], dtype=tf.float32), name="W")
+        W1 = tf.Variable(tf.random_normal([num_param, 1], dtype=tf.float32), name="W")
         b1 = tf.Variable(tf.zeros([batch_size, 1], dtype=tf.float32), name="b")
         y = tf.add(tf.matmul(input_parameter, W1), b1)
         
@@ -156,6 +166,13 @@ def train_one_epoch(sess, cost_op, train_op, num_batches, input_parameter, input
      for batch_idx in range(num_batches):
         start_idx = batch_idx * batch_size
         end_idx = (batch_idx+1) * batch_size
+        
+        cost,_ = sess.run([cost_op, train_op], feed_dict={input_parameter: input_param[start_idx:end_idx], input_volume: input_vol[start_idx:end_idx]})
+        
+        #concat, pred = sess.run([input_param_concat, y], feed_dict={input_parameter: input_param[start_idx:end_idx], input_volume: input_vol[start_idx:end_idx]})
+        #print("input volume: ", input_vol[start_idx:end_idx])
+        #print("predicted value: ", pred)
+        '''
         try:
             cost,_ = sess.run([cost_op, train_op], feed_dict={input_parameter: input_param[start_idx:end_idx], input_volume: input_vol[start_idx:end_idx]})
             
@@ -170,7 +187,7 @@ def train_one_epoch(sess, cost_op, train_op, num_batches, input_parameter, input
             print("input error: ")
             print("input parameter: ", input_param[start_idx:end_idx])
             print("input volume: ", input_volume[start_idx:end_idx])
-            
+        '''
         print("loss: ", cost)
         
 def eval_one_epoch(sess, cost_op, test_batches, input_parameter, test_param, input_volume, test_vol):
@@ -184,7 +201,7 @@ def eval_one_epoch(sess, cost_op, test_batches, input_parameter, test_param, inp
 last_cost = 0
 alpha = 0.4
 batch_size = 10
-max_epochs = 2
+max_epochs = 10000
 tolerance = 1e-3
 
     
