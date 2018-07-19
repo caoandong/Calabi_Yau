18dd299383bbed02c8f2c81eaa4f7c21842b7ad1
Cao <antonio.cao@yale.edu>
2018-07-19 13:25:20 +0000

    README

--- machine_learning/fully_connected/multi_linear.py
+++ machine_learning/fully_connected/multi_linear.py
@@ -27,6 +27,55 @@ def load_input(train_path):
     
     return train_x, train_y
 
+def load_input_extend(train_path):
+    train = open(train_path, 'r')
+    train_x = []
+    train_y = []
+    for line in train:
+        data = eval(line)
+        if data[1] > 0:
+            p1 = data[0][0]
+            p2 = data[0][1]
+            p3 = data[0][2]
+            p = p1**2
+            data[0].append(p)
+            p = p2**2
+            data[0].append(p)
+            p = p3**2
+            data[0].append(p)
+            p = p1*p2
+            data[0].append(p)
+            p = p1*p3
+            data[0].append(p)
+            p = p2*p3
+            data[0].append(p)
+            p = p1**3
+            data[0].append(p)
+            p = p2**3
+            data[0].append(p)
+            p = p3**3
+            data[0].append(p)
+            p = (p1**2)*p2
+            data[0].append(p)
+            p = (p1**2)*p3
+            data[0].append(p)
+            p = p1*(p2**2)
+            data[0].append(p)
+            p = p1*(p3**2)
+            data[0].append(p)
+            p = (p2**2)*p3
+            data[0].append(p)
+            p = (p3**2)*p2
+            data[0].append(p)
+            p = p1*p2*p3 
+            data[0].append(p)
+            train_x.append(data[0])
+            inv_vol = float(data[1])**(-1)
+            train_y.append(inv_vol)
+    print ('Number of data: ', len(train_x))
+    
+    return train_x, train_y
+
 def expand_2(input_param_1, batch_size, num_param):
     for row in range(batch_size):
         param_tmp = input_param_1[row,:]
@@ -173,7 +222,42 @@ def mat_mul_2(param, batch_size, num_param):
         b2 = tf.Variable(tf.zeros([batch_size, 1], dtype=tf.float32), name="b2")
         y = tf.add(tf.matmul(y, W2), b2)
         
-        cost_op = tf.reduce_mean(tf.multiply(tf.pow(tf.pow(y,-1)-tf.pow(input_volume,-1), 2), y))
+        #cost_op = tf.reduce_mean(tf.multiply(tf.pow(tf.pow(y,-1)-tf.pow(input_volume,-1), 2), y))
+        cost_op = tf.reduce_mean(tf.pow(y-input_volume, 2))
+
+    return y, cost_op, input_parameter, input_volume
+
+def mat_mul_3(param, batch_size, num_param):
+    # TODO: make this more general
+    with tf.variable_scope("mat_mul_3", reuse=True):
+        input_parameter = tf.placeholder(name="input_parameter", dtype=tf.float32, shape=(batch_size, num_param))
+        input_volume = tf.placeholder(name="input_volume", dtype=tf.float32, shape=(batch_size))
+        
+        # Layer 1:
+        # Input: (batch_size, num_param)
+        # W1: (num_param, 10), b: (batch_size, 10)
+        # Output: (batch_size, 10)
+        W1 = tf.Variable(tf.random_normal([num_param, 10], dtype=tf.float32), name="W1")
+        y = tf.matmul(input_parameter, W1)
+        
+        # Layer 2:
+        # Input: (batch_size, 10)
+        # W1: (10, 5), b: (batch_size, 5)
+        # Output: (batch_size, 5)
+        W2 = tf.Variable(tf.random_normal([10, 5], dtype=tf.float32), name="W2")
+        y = tf.matmul(y, W2)
+        
+        # Layer 3:
+        # Input: (batch_size, 5)
+        # W1: (5, 1), b: (batch_size, 1)
+        # Output: (batch_size, 1)
+        W3 = tf.Variable(tf.random_normal([5, 1], dtype=tf.float32), name="W3")
+        b3 = tf.Variable(tf.zeros([batch_size, 1], dtype=tf.float32), name="b3")
+        y = tf.add(tf.matmul(y, W3), b3)
+        
+        #cost_op = tf.reduce_mean(tf.pow(tf.pow(y,-1)-tf.multiply(tf.pow(input_volume,-1), y), 2))
+        cost_op = tf.reduce_mean(tf.pow(y-input_volume, 2))
+        #cost_op = tf.reduce_mean(tf.multiply(tf.pow(y-input_volume, 2), tf.pow(y, -3/4)))
 
     return y, cost_op, input_parameter, input_volume
 
@@ -197,6 +281,8 @@ def train(data, vol):
     print('number of test: ', num_test)
     
     num_batches = int(num_train/batch_size)
+    print('batch_size: ', batch_size)
+    print('number of batches: ', num_batches)
     test_batches = int(num_test/batch_size)
     input_param = np.array(input_data)
     input_vol = np.array(input_vol)
@@ -204,7 +290,7 @@ def train(data, vol):
     test_vol = np.array(test_vol)
     
     #y = fl_linear(input_param)
-    y, cost_op, input_parameter, input_volume, input_param_concat = mat_mul(input_param, batch_size, num_param)
+    y, cost_op, input_parameter, input_volume = mat_mul_3(input_param, batch_size, num_param)
     
     learning_rate = tf.Variable(0.5, trainable=False)
     optimizer = tf.train.AdamOptimizer(learning_rate)
@@ -219,57 +305,62 @@ def train(data, vol):
         sess.run(tf.assign(learning_rate, alpha))
         for epoch in range(1, max_epochs):
             print('epoch: ', epoch)
-            train_one_epoch(sess, cost_op, train_op, num_batches, input_parameter, input_param, input_volume, input_vol, input_param_concat, y)
+            train_one_epoch(sess, cost_op, train_op, num_batches, input_parameter, input_param, input_volume, input_vol, y)
             eval_one_epoch(sess, cost_op, test_batches, input_parameter, test_param, input_volume, test_vol)
             
             if epoch % 10 == 0:
-                path = "log/model_2.ckpt"
+                path = "log/model_3.ckpt"
                 save_path = saver.save(sess, path)
                 print("Saved.")
             
         print('Done.')
         
-        collection = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='mat_mul')
+        collection = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='mat_mul_3')
         for x in collection:
-            if x.name == 'mat_mul/W:0' or x.name == 'mat_mul/b:0':
+            if x.name == 'mat_mul_3/W1:0' or x.name == 'mat_mul_3/W2:0'or x.name == 'mat_mul_3/W3:0' or x.name == 'mat_mul_3/b3:0':
                 print("name: ", x.name)
                 print("variables: ", sess.run(x))
+                output.write("name: %s\n" % x.name)
+                output.write("variable: %s\n" % str(sess.run(x)))
             
-
-def train_one_epoch(sess, cost_op, train_op, num_batches, input_parameter, input_param, input_volume, input_vol, input_param_concat, y):
-     for batch_idx in range(num_batches):
+def train_one_epoch(sess, cost_op, train_op, num_batches, input_parameter, input_param, input_volume, input_vol, y):
+    #print('batches: ', num_batches)
+    for batch_idx in range(num_batches):
         start_idx = batch_idx * batch_size
         end_idx = (batch_idx+1) * batch_size
-        
-        cost,_ = sess.run([cost_op, train_op], feed_dict={input_parameter: input_param[start_idx:end_idx], input_volume: input_vol[start_idx:end_idx]})
-        
+
+        batch_cost,_ = sess.run([cost_op, train_op], feed_dict={input_parameter: input_param[start_idx:end_idx], input_volume: input_vol[start_idx:end_idx]})
+        print("loss: ", batch_cost)
+        output.write("loss: %f\n" % batch_cost)
+
         #concat, pred = sess.run([input_param_concat, y], feed_dict={input_parameter: input_param[start_idx:end_idx], input_volume: input_vol[start_idx:end_idx]})
         #print("input volume: ", input_vol[start_idx:end_idx])
         #print("predicted value: ", pred)
         '''
         try:
             cost,_ = sess.run([cost_op, train_op], feed_dict={input_parameter: input_param[start_idx:end_idx], input_volume: input_vol[start_idx:end_idx]})
-            
-            
+
+
             #print("input parameter: ", input_param[start_idx:end_idx])
             #concat, pred = sess.run([input_param_concat, y], feed_dict={input_parameter: input_param[start_idx:end_idx], input_volume: input_vol[start_idx:end_idx]})
             #print("parameter concatinated: ", concat)
             print("input volume: ", input_vol[start_idx:end_idx])
             print("predicted value: ", pred)
-            
+
         except:
             print("input error: ")
             print("input parameter: ", input_param[start_idx:end_idx])
             print("input volume: ", input_volume[start_idx:end_idx])
         '''
-        print("loss: ", cost)
+        
         
 def eval_one_epoch(sess, cost_op, test_batches, input_parameter, test_param, input_volume, test_vol):
     for batch_idx in range(test_batches):
         start_idx = batch_idx * batch_size
         end_idx = (batch_idx+1) * batch_size
-        cost = sess.run(cost_op, feed_dict={input_parameter: test_param[start_idx:end_idx], input_volume: test_vol[start_idx:end_idx]})
-        print("test loss: ", cost)
+        batch_cost = sess.run(cost_op, feed_dict={input_parameter: test_param[start_idx:end_idx], input_volume: test_vol[start_idx:end_idx]})
+        print("test loss: ", batch_cost)
+        output.write("test loss: %f\n" % batch_cost)
 
 # Parameters:
 last_cost = 0
@@ -278,8 +369,9 @@ batch_size = 10
 max_epochs = 10000
 tolerance = 1e-3
 
-    
 train_path = '/home/carnd/CYML/output/train/cylinder/lift_1_to_50.txt'
-train_data, train_vol = load_input(train_path)
+out_path = '/home/carnd/CYML/output/train/cylinder/loss/lift_1_to_50.txt'
+output = open(out_path, 'w')
+train_data, train_vol = load_input_extend(train_path)
 print ('sample data: ', train_data[0])
 train(train_data, train_vol)
